da68aa4fea7cdc8ea33cf0c522395b3cd7be3996
tarydenise <tarydenise@gmail.com>
2024-12-14 04:32:59 -0800

    W5 Inheritance Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Hello Learning05 World!\n");
+
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary() + "\n");
+        
+        MathAssigment mathAssigment = new MathAssigment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssigment.GetSummary());
+        Console.WriteLine(mathAssigment.GetHomeworkList() + "\n");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation() + "\n");
     }
 }
